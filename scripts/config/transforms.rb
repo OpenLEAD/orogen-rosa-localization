8f75f6e1f13d1cd28ad79f361ddbc0241a240599
alcantara09 <alcantara@poli.ufrj.br>
2014-12-19 14:46:26 -0200

    Fixing typo from rock page
    Eigen::Vector3 instead of Eigen::Vector3d

--- scripts/config/transforms.rb
+++ scripts/config/transforms.rb
@@ -1,12 +1,12 @@
 # The frame names in this file are arbitrary !
-static_transform Eigen::Vector3d.new(0.3, 0, 0),
+static_transform Eigen::Vector3.new(0.3, 0, 0),
   "sonar" => "ptu_kongsberg_moving"
 
-static_transform Eigen::Vector3d.new(0.3, 0, 0),
+static_transform Eigen::Vector3.new(0.3, 0, 0),
   "body" => "ptu_kongsberg_base"
 
 dynamic_transform "ptu.orientation_samples",
   "ptu_kongsberg_base" => "ptu_kongsberg_moving"
 
-static_transform Eigen::Vector3d.new(0.3, 0, 0),
+static_transform Eigen::Vector3.new(0.3, 0, 0),
     "body" => "depth_sensor_frame"
